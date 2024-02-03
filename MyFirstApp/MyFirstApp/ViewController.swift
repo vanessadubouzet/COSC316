137c40709a8b301816591150b24f66d6891a6547
vanessadubouzet <vanessadubouzet@gmail.com>
2024-02-03 00:03:45 -0800

    Added UI components and functionality

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBOutlet var inputTextfield: UITextField!
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBAction func GoForIt(_ sender: Any) {
+        outputLabel.text = "Hello, " + inputTextfield.text!
+        image.image = UIImage(named: "norway")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.inputTextfield.delegate = self
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        inputTextfield.resignFirstResponder()
+        return true
+    }
 }
 
