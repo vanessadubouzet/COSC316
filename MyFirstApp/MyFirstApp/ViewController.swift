084e76a0b5972f8398b9e3764a8db558bfc08293
vanessadubouzet <vanessadubouzet@gmail.com>
2024-02-02 23:10:43 -0800

    Added base project files

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Vanessa Dubouzet on 2024-02-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
