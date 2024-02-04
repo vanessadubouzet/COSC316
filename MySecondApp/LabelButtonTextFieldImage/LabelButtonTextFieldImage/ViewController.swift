fb4dea5738f7bc866841f99b3946339b9cb25794
vanessadubouzet <vanessadubouzet@gmail.com>
2024-02-03 23:03:38 -0800

    Added Slider and Segment Control
    Image output will change depending on output

--- MySecondApp/LabelButtonTextFieldImage/LabelButtonTextFieldImage/ViewController.swift
+++ MySecondApp/LabelButtonTextFieldImage/LabelButtonTextFieldImage/ViewController.swift
@@ -10,19 +10,57 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate  {
 
-    @IBOutlet weak var outputLabel: UILabel!
     @IBOutlet weak var inputTextfield: UITextField!
     
-    @IBAction func getInput(_ sender: Any) {
-       outputLabel.text = "Hello, " + inputTextfield.text!
+    
+    @IBOutlet weak var imageSlider: UISlider!
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    @IBAction func getImage(_ sender: Any) {
+        let imageNum = segmentControl.selectedSegmentIndex
+        switch imageNum {
+        case 0:
+        imageView.image = UIImage(named: "norway")
+        break
+        case 1:
+        imageView.image = UIImage(named: "italy")
+        break
+        case 2:
+        imageView.image = UIImage(named: "forest_park")
+        break
+        default:
+        break
+        }
+    }
+    
+    @IBAction func imageSlider(_ sender: Any) {
+        if(imageSlider.value < 0.34) {
+            imageView.image = UIImage(named: "norway")
+        }
+        else if (imageSlider.value > 0.67) {
+            imageView.image = UIImage(named: "forest_park")
+        }
+        else {
+            imageView.image = UIImage(named: "italy")
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-   
-        self.inputTextfield.delegate = self    //***
-        self.inputTextfield.keyboardType = .numberPad
+    }
+    
+    @IBAction func setValue(_sender: Any) {
+        if(imageSlider.value < 0.34) {
+            imageView.image = UIImage(named: "norway")
+        }
+        else if (imageSlider.value > 0.67) {
+            imageView.image = UIImage(named: "forest_park")
+        }
+        else {
+            imageView.image = UIImage(named: "italy")
+        }
     }
 
     override func didReceiveMemoryWarning() {
