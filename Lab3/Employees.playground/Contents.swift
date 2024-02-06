87e41b10b1bb779f3e1311667109539cad2346bb
vanessadubouzet <vanessadubouzet@gmail.com>
2024-02-05 23:35:34 -0800

    Created Volunteer Class

--- Lab3/Employees.playground/Contents.swift
+++ Lab3/Employees.playground/Contents.swift
@@ -4,9 +4,9 @@ class Employee {
     var EmployeeName: String
     var EmployeeID: String
     var JobTitle: String
-    var YearsOfService: String
+    var YearsOfService: Int
     
-    init(Name: String, ID: String, JobTitle: String, YearsOfService: String) {
+    init(Name: String, ID: String, JobTitle: String, YearsOfService: Int) {
         self.EmployeeName = Name
         self.EmployeeID = ID
         self.JobTitle = JobTitle
@@ -21,3 +21,15 @@ class Employee {
         return "Name: \(EmployeeName)\n ID: \(EmployeeID)\n Title: \(JobTitle)\n Years of Service: \(YearsOfService)"
     }
 }
+
+class Volunteer : Employee{
+    var reimbursment: Double = 100.00;
+    
+    override func toString() -> String {
+        return "Name: \(EmployeeName)\n ID: \(EmployeeID)\n Title: \(JobTitle)\n Years of Service: \(YearsOfService)\n Reimburstment amount $\(reimbursment)\n Thank you for your volunteer work and time!"
+    }
+}
+
+var emp1 = Volunteer(Name: "Peter Long", ID: "112-22-3011", JobTitle: "Volunteer Worker", YearsOfService: 5)
+print(emp1.toString())
+print("---------------------------------------------------------")
