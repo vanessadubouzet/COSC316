57572ddb02e2acbbde13777bee7371fc93fff067
vanessadubouzet <vanessadubouzet@gmail.com>
2024-02-07 23:16:51 -0800

    Adding Control Objects

---
+++ Control Fun/Control Fun/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Control Fun
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var leftSwitch: UISwitch!
+    @IBOutlet weak var rightSwitch: UISwitch!
+    @IBOutlet weak var doSomethingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        sliderLabel.text = "50"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
+
