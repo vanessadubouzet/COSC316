e505ffc16dd1787791aa87eb50b47402bc241fd4
vanessadubouzet <vanessadubouzet@gmail.com>
2024-02-08 00:11:39 -0800

    added alert pop up box when button clicked

--- Control Fun/Control Fun/ViewController.swift
+++ Control Fun/Control Fun/ViewController.swift
@@ -40,5 +40,45 @@ class ViewController: UIViewController {
         sliderLabel.text = "\(lroundf(sender.value))"
     }
     
+    @IBAction func onSwitchChanged(_ sender: UISwitch) {
+        let setting = sender.isOn // true or false
+        leftSwitch.setOn(setting, animated: true)
+        rightSwitch.setOn(setting, animated: true)
+    }
+    
+    @IBAction func toggleControls(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            leftSwitch.isHidden = false
+            rightSwitch.isHidden = false
+            doSomethingButton.isHidden = true
+        } else {
+            leftSwitch.isHidden = true
+            rightSwitch.isHidden = true
+            doSomethingButton.isHidden = false
+        }
+    }
+    
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let controller = UIAlertController(title: "Are You Sure?", message:nil, preferredStyle: .actionSheet)
+        let yesAction = UIAlertAction(title: "Yes, I'm sure!", style: .destructive, handler: {
+            action in
+            
+            let msg = self.nameField.text!.isEmpty ? "You can breathe easy, everything went OK."
+            : "You can breathe easy, \(String(describing:self.nameField.text))," + "everything went OK."
+            
+            let controller2 = UIAlertController(title: "Something was done", message: msg, preferredStyle: .alert)
+            
+            let cancelAction = UIAlertAction(title: "Phew!",style:.cancel, handler:nil)
+            controller2.addAction(cancelAction)
+            self.present(controller2, animated: true, completion:nil)
+        })
+        
+        let noAction = UIAlertAction(title: "No Way!", style: .cancel, handler:nil)
+        
+        controller.addAction(yesAction)
+        controller.addAction(noAction)
+        
+        present(controller, animated: true, completion: nil)
+    }
 }
 
