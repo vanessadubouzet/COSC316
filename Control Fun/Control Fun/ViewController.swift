0dc279f8da38a32b124394fa42db7a8774bb9748
vanessadubouzet <vanessadubouzet@gmail.com>
2024-02-07 23:42:24 -0800

    Add tapgesture

--- Control Fun/Control Fun/ViewController.swift
+++ Control Fun/Control Fun/ViewController.swift
@@ -27,7 +27,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func textFieldDoneEditing(sender:UITextField) {
+        sender.resignFirstResponder()
+    }
     
+    @IBAction func onTapGestureRecognized(_sender: AnyObject) {
+        nameField.resignFirstResponder()
+        numberField.resignFirstResponder()
+    }
     
 }
 
