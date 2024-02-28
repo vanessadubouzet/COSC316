24847031d9f4f6d8e2f26804f0843c3a88c92279
vanessadubouzet <vanessadubouzet@gmail.com>
2024-02-28 00:06:44 -0800

    Finished Navigation Control with Table View

--- NavigationControllerTableView/NavigationControllerTableView/TableViewController.swift
+++ NavigationControllerTableView/NavigationControllerTableView/TableViewController.swift
@@ -5,4 +5,65 @@
 //  Created by Vanessa Dubouzet on 2024-02-27.
 //
 
-import Foundation
+import UIKit
+
+struct Headline {
+    var id : Int
+    var title : String
+    var text : String
+    var image : String
+}
+
+class TableViewController: UITableViewController {
+    
+    var headlines = [
+        Headline(id: 1, title: "Apple", text: "The apple tree (Malus domestica) is a tree that grows fruit in the rose family best known for its juicy, tasty fruit. ", image: "Apple"),
+        Headline(id: 2, title: "Banana", text: "Most banana plants are grown for their fruits, which botanically are a type of berry.", image: "Banana"),
+        Headline(id: 3, title: "Cantaloupe", text: "The cantaloupe, rockmelon (Australia), sweet melon, or spanspek (South Africa) is a melon that is a variety of the muskmelon species (Cucumis melo) from the family Cucurbitaceae.", image: "Cantaloupe"),
+        ]
+    
+//    override func numberOfSections (in tableView: UITableView) -> Int {
+//    return headlines.count //exact number to be determined later
+//     }
+     
+     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return headlines.count // exact number to be determined later
+     // with the data source we used
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+    UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+                let headline = headlines[indexPath.row]
+                cell.textLabel?.text = headlines[indexPath.row].title
+                cell.detailTextLabel?.text = headline.text
+                cell.imageView?.image = UIImage(named: headline.image)
+    return cell
+     }
+    
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) ->
+//    String? {
+//     return "Section \(section)"
+//     }
+    
+    func tableView(tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+
+     performSegue(withIdentifier: "ToDetailSegue", sender: self)
+
+     }
+
+     override func prepare (for segue: UIStoryboardSegue, sender: Any?){
+         if segue.identifier == "ToDetailSegue" {
+         let sdest = segue.destination as? ViewController
+         let indexPath = tableView.indexPathForSelectedRow
+         let titleString = headlines[(indexPath?.row)!].title
+
+         sdest?.titleStringViaSegue = titleString
+         }
+     }
+
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         // Do any additional setup after loading the view.
+         }
+    }
--- NavigationControllerTableView/NavigationControllerTableView/ViewController.swift
+++ NavigationControllerTableView/NavigationControllerTableView/ViewController.swift
@@ -7,46 +7,31 @@
 
 import UIKit
 
-struct Headline {
-    var id : Int
-    var title : String
-    var text : String
-    var image : String
-}
+class ViewController: UIViewController {
+    var titleStringViaSegue : String?
+    
+    @IBOutlet weak var viewImage: UIImageView!
+    @IBOutlet weak var viewLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+     super.viewWillAppear(animated)
 
-class TableViewController: UITableViewController {
-    
-    var headlines = [
-        Headline(id: 1, title: "Apple", text: "The apple tree (Malus domestica) is a tree that grows fruit in the rose family best known for its juicy, tasty fruit. ", image: "Apple"),
-        Headline(id: 2, title: "Banana", text: "Most banana plants are grown for their fruits, which botanically are a type of berry.", image: "Banana"),
-        Headline(id: 3, title: "Cantaloupe", text: "The cantaloupe, rockmelon (Australia), sweet melon, or spanspek (South Africa) is a melon that is a variety of the muskmelon species (Cucumis melo) from the family Cucurbitaceae.", image: "Cantaloupe"),
-        ]
-    
-//    override func numberOfSections (in tableView: UITableView) -> Int {
-//    return headlines.count //exact number to be determined later
-//     }
-     
-     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return headlines.count // exact number to be determined later
-     // with the data source we used
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
-    UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
-                let headline = headlines[indexPath.row]
-                cell.textLabel?.text = headlines[indexPath.row].title
-                cell.detailTextLabel?.text = headline.text
-                cell.imageView?.image = UIImage(named: headline.image)
-    return cell
+     if titleStringViaSegue == "Cantaloupe"
+     {
+     viewImage.image = UIImage(named: "Cantaloupe")
+     }
+     else if titleStringViaSegue == "Banana" {
+     viewImage.image = UIImage (named: "Banana")
+     }
+     else {
+     viewImage.image = UIImage (named: "Apple")
+     }
+
+     viewLabel.text = titleStringViaSegue
+
      }
     
-//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) ->
-//    String? {
-//     return "Section \(section)"
-//     }
-
-     override func viewDidLoad() {
+    override func viewDidLoad() {
          super.viewDidLoad()
          // Do any additional setup after loading the view.
          }
