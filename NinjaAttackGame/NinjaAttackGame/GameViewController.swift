ccb0f7902d99193cbe8bbd9dc36ccb26b59fe093
vanessadubouzet <vanessadubouzet@gmail.com>
2024-03-05 12:37:45 -0800

    NinjiaAttackGame

---
+++ NinjaAttackGame/NinjaAttackGame/GameViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GameViewController.swift
+//  NinjaAttackGame
+//
+
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+
+  
+}
